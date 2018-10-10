ce98cf607566b01bb5103cd4cb90e2069dd7d9f1
Alex Dillhoff <ajdillhoff@gmail.com>
2018-10-10 12:44:14 -0500

    Finished demo for PoseNet.

--- colorhandpose3d/utils/general.py
+++ colorhandpose3d/utils/general.py
@@ -1,5 +1,6 @@
 import math
 
+import numpy as np
 import torch
 import torch.nn.functional as F
 from roi_align.crop_and_resize import CropAndResizeFunction
@@ -178,3 +179,87 @@ def crop_image_from_xy(image, crop_location, crop_size, scale=1.0):
     image_crops = CropAndResizeFunction(crop_size, crop_size, 0)(image, boxes, box_ind)
 
     return image_crops
+
+
+def detect_keypoints(scoremaps):
+    """Detect keypoints using the scoremaps provided by PoseNet.
+
+    Args:
+        scoremaps - numpy array (num_scoremaps x H x W): Scoremaps of a single
+            sample.
+
+    Returns:
+        keypoint_coords - numpy array (num_scoremaps x 2): Coordinates of each
+            keypoint.
+    """
+
+    s = scoremaps.shape
+    assert len(s) == 3, "Input must be 3D."
+
+    keypoint_coords = np.zeros((s[0], 2))
+
+    for i in range(s[0]):
+        v, u = np.unravel_index(np.argmax(scoremaps[i]), (s[1], s[2]))
+        keypoint_coords[i, 0] = v
+        keypoint_coords[i, 1] = u
+
+    return keypoint_coords
+
+
+def plot_hand(coords_hw, axis, color_fixed=None, linewidth='1'):
+    """Plots the 2D pose estimates into a matplotlib figure."""
+    colors = np.array([[0., 0., 0.5],
+                       [0., 0., 0.73172906],
+                       [0., 0., 0.96345811],
+                       [0., 0.12745098, 1.],
+                       [0., 0.33137255, 1.],
+                       [0., 0.55098039, 1.],
+                       [0., 0.75490196, 1.],
+                       [0.06008855, 0.9745098, 0.90765338],
+                       [0.22454143, 1., 0.74320051],
+                       [0.40164453, 1., 0.56609741],
+                       [0.56609741, 1., 0.40164453],
+                       [0.74320051, 1., 0.22454143],
+                       [0.90765338, 1., 0.06008855],
+                       [1., 0.82861293, 0.],
+                       [1., 0.63979666, 0.],
+                       [1., 0.43645606, 0.],
+                       [1., 0.2476398, 0.],
+                       [0.96345811, 0.0442992, 0.],
+                       [0.73172906, 0., 0.],
+                       [0.5, 0., 0.]])
+
+    # define connections and colors of the bones
+    bones = [((0, 4), colors[0, :]),
+             ((4, 3), colors[1, :]),
+             ((3, 2), colors[2, :]),
+             ((2, 1), colors[3, :]),
+
+             ((0, 8), colors[4, :]),
+             ((8, 7), colors[5, :]),
+             ((7, 6), colors[6, :]),
+             ((6, 5), colors[7, :]),
+
+             ((0, 12), colors[8, :]),
+             ((12, 11), colors[9, :]),
+             ((11, 10), colors[10, :]),
+             ((10, 9), colors[11, :]),
+
+             ((0, 16), colors[12, :]),
+             ((16, 15), colors[13, :]),
+             ((15, 14), colors[14, :]),
+             ((14, 13), colors[15, :]),
+
+             ((0, 20), colors[16, :]),
+             ((20, 19), colors[17, :]),
+             ((19, 18), colors[18, :]),
+             ((18, 17), colors[19, :])]
+
+    for connection, color in bones:
+        coord1 = coords_hw[connection[0], :]
+        coord2 = coords_hw[connection[1], :]
+        coords = np.stack([coord1, coord2])
+        if color_fixed is None:
+            axis.plot(coords[:, 1], coords[:, 0], color=color, linewidth=linewidth)
+        else:
+            axis.plot(coords[:, 1], coords[:, 0], color_fixed, linewidth=linewidth)
